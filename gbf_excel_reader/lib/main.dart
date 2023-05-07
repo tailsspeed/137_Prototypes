2dd69000f2a317942308c3fac8d19b30c673d200
ktheart <kt.heart@gmail.com>
2023-05-07 16:43:21 -0700

    cleaned up code for Character instances

--- gbf_excel_reader/lib/main.dart
+++ gbf_excel_reader/lib/main.dart
@@ -20,56 +20,23 @@ void main() {
       for (var row in decoder.tables[table]!.rows) { // print contents of rows
         List character = row;
 
-        String name = character[0];
-        var number = character[1] is double ? character[1].round() : character[1];
-        String rarity = character[2];
-        String element = character[3];
-        String style = character[4];
-        String race = character[5];
-        String sex = character[6];
-        var uncap =  character[7] is double ? character[7].round() : character[7];
-        var hp =  character[8] is double ? character[8].round() : character[8];
-        var atk =  character[9] is double ? character[9].round() : character[9];
-        String weapon1 = character[10];
-        String? weapon2 = character[11];
-        String wikiLink = character[12];
-
         Character gbfCharacter;
         gbfCharacter = Character(
-          name:     name,
-          number:   number, 
-          rarity:   rarity, 
-          element:  element, 
-          style:    style, 
-          race:     race, 
-          sex:      sex,
-          uncap:    uncap, 
-          hp:       hp, 
-          atk:      atk, 
-          weapon1:  weapon1, 
-          weapon2:  weapon2, 
-          wikiLink: wikiLink,  
+          name:     character[0],
+          number:   character[1], 
+          rarity:   character[2], 
+          element:  character[3], 
+          style:    character[4], 
+          race:     character[5], 
+          sex:      character[6],
+          uncap:    character[7], 
+          hp:       character[8], 
+          atk:      character[9], 
+          weapon1:  character[10], 
+          weapon2:  character[11], 
+          wikiLink: character[12],  
         );
-        // print('$gbfCharacter.name');
-
-        // gbfCharacter.toString();
-
         charList.add(gbfCharacter);
-
-        
-        // print('From instance: $name');
-        // print('From class: $gbfCharacter.name');
-
-        // print('Name: $name');
-        // print('Number: $number');
-        // print('Rarity: $rarity');
-        // print('Element: $element');
-        // print('Style: $style');
-        // print('Race: $race');
-        // print('Sex: $sex');
-        // print('Uncap: $uncap');
-        // print('HP: $hp');
-        // print('ATK: $atk');
       }
     }
   for (var gbf_char in charList) {
